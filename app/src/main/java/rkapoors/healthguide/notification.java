f8bd319118b92a2d75dd743bcb8586d93049587e
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2018-05-04 22:01:05 +0530

    pre internal ppt

--- app/src/main/java/rkapoors/healthguide/notification.java
+++ app/src/main/java/rkapoors/healthguide/notification.java
@@ -1,9 +1,52 @@
 package rkapoors.healthguide;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class notification extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+    Button bt;
+    ListView lst;
+
+    ArrayList<String> messagelist;
+    ArrayAdapter<String> adapter;
+
+    String disp = "",prevdt="", uidofuser="";
+    RelativeLayout relativeLayout;
+
+    Date firebasetithi, prevdate;
+    DateFormat df;
+    SimpleDateFormat dateFormatter;
+
+    int flag=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +57,123 @@ public class notification extends AppCompatActivity {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         if(actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        database=FirebaseDatabase.getInstance();
+        databaseReference=database.getReference();
+
+        bt = (Button)findViewById(R.id.rldbt);
+        lst = (ListView)findViewById(R.id.msglist);
+        relativeLayout = (RelativeLayout)findViewById(R.id.layout);
+
+        messagelist = new ArrayList<>();
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,messagelist);
+        lst.setAdapter(adapter);
+
+        dateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+5:30"));
+        cal.add(Calendar.DATE,-8);              //go back 8 days
+        prevdt = dateFormatter.format(cal.getTime());
+
+        df = new SimpleDateFormat("yyyy-MM-dd",Locale.US);
+        try {
+            prevdate = df.parse(prevdt);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) uidofuser = user.getUid();
+
+        fetchrecord task = new fetchrecord(notification.this);
+        task.execute();
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag=0;
+                messagelist = new ArrayList<>();
+                fetchrecord task = new fetchrecord(notification.this);
+                task.execute();
+            }
+        });
+    }
+
+    private class fetchrecord extends AsyncTask<Void, Void, Void>{
+        private ProgressDialog pd;
+
+        public fetchrecord(notification activity){
+            pd = new ProgressDialog(activity);
+        }
+
+        @Override
+        protected void onPreExecute(){
+            pd.setMessage("Loading...");
+            pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params){
+            try{
+
+                databaseReference.child("users").child(uidofuser).child("notifs").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(DataSnapshot ds : dataSnapshot.getChildren()){
+                            String tithi = ds.getKey();
+                            try {
+                                firebasetithi = df.parse(tithi);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+
+                            if(prevdate.compareTo(firebasetithi)>=0){
+                                ds.getRef().setValue(null);
+                            }
+
+                            else{
+                                for(DataSnapshot dts : ds.getChildren()){
+                                    messagelist.add(tithi+"\n\n"+dts.child("msg").getValue(String.class));
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Snackbar.make(relativeLayout,"Connection Lost. Try Again.",Snackbar.LENGTH_LONG).show();
+                    }
+                });
+                flag=1;
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result){
+            super.onPostExecute(result);
+            Handler handler = new Handler();
+
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    if(flag==1){
+                        adapter.notifyDataSetChanged();
+                        if(messagelist.size()==0){
+                            Snackbar.make(relativeLayout,"No new notifications.",Snackbar.LENGTH_LONG).show();
+                        }
+                    }
+                    else{
+                        Snackbar.make(relativeLayout,"Something went wrong. Try again.",Snackbar.LENGTH_LONG).show();
+                    }
+
+                    pd.dismiss();
+                }
+            },5000);
         }
     }
 
